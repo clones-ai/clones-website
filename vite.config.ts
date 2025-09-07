2390f3e38b530337c36f73f23e700967f640c085
redDwarf03 <sylvain@archethic.net>
2025-09-07 18:46:07 +0200

    Enhancement: Add deployment scripts and GitHub Actions workflow for GitHub Pages. Update Vite configuration for production base path and include predeploy script in package.json.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/clones-website/' : '/',
   esbuild: {
     target: 'es2020',
     minify: true,
